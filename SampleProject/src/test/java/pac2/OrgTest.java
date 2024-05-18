85ba8622bbf966db4f40c162dd26df580d1e7b53
lenovo <lenovo@LAPTOP-IL8SBD7O>
2024-05-18 02:23:45 -0700

    contact test org test comitted

---
+++ SampleProject/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,16 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute create org test");
+	}
+
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("execute modify org");
+	}
+}
